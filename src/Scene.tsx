09e16738ca35edb82bffafbb35296020c24accb7
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2026-01-04 15:09:04 +0300

    Update Scene.tsx

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,19 +1,26 @@
 
-import { ThreeCanvas } from "@remotion/three";
-import { AbsoluteFill, useVideoConfig } from "remotion";
 import React from "react";
+import { AbsoluteFill, useVideoConfig } from "remotion";
+import { ThreeCanvas } from "@remotion/three";
 import { Phone } from "./Phone";
 
 export const Scene: React.FC = () => {
   const { width, height } = useVideoConfig();
 
   return (
-    <AbsoluteFill style={{ backgroundColor: "#ffffff" }}> {/* bright white background */}
-      <ThreeCanvas width={width} height={height} frameloop="always" linear>
-        {/* Clean, cinematic lighting */}
-        <ambientLight intensity={0.8} />
-        <directionalLight position={[5, 10, 5]} intensity={1.2} />
-        <directionalLight position={[-5, -5, 5]} intensity={0.8} />
+    <AbsoluteFill style={{ backgroundColor: "#ffffff" }}>
+      <ThreeCanvas
+        width={width}
+        height={height}
+        frameloop="always"
+        linear
+        camera={{ fov: 40, position: [0, 0, 6] }}
+      >
+        {/* CINEMATIC LIGHTING */}
+        <ambientLight intensity={0.9} />
+        <directionalLight position={[5, 8, 5]} intensity={1.2} />
+        <directionalLight position={[-5, -5, 5]} intensity={0.6} />
+
         <Phone />
       </ThreeCanvas>
     </AbsoluteFill>
