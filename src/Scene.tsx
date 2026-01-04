2c55c307e24234cbe0f2034b686ed8dc504f7490
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2026-01-04 15:18:21 +0300

    Update Scene.tsx

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,27 +1,28 @@
 
 import React from "react";
-import { AbsoluteFill, useVideoConfig } from "remotion";
+import { AbsoluteFill } from "remotion";
 import { ThreeCanvas } from "@remotion/three";
 import { Phone } from "./Phone";
+import { MediaItem } from "./media-sequence";
 
-export const Scene: React.FC = () => {
-  const { width, height } = useVideoConfig();
-
+export const Scene: React.FC<{
+  mediaSequence: MediaItem[];
+  videoFrameMap: Record<string, number>;
+}> = ({ mediaSequence, videoFrameMap }) => {
   return (
     <AbsoluteFill style={{ backgroundColor: "#ffffff" }}>
       <ThreeCanvas
-        width={width}
-        height={height}
-        frameloop="always"
         linear
         camera={{ fov: 40, position: [0, 0, 6] }}
       >
-        {/* CINEMATIC LIGHTING */}
         <ambientLight intensity={0.9} />
         <directionalLight position={[5, 8, 5]} intensity={1.2} />
         <directionalLight position={[-5, -5, 5]} intensity={0.6} />
 
-        <Phone />
+        <Phone
+          mediaSequence={mediaSequence}
+          videoFrameMap={videoFrameMap}
+        />
       </ThreeCanvas>
     </AbsoluteFill>
   );
