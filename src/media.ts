70acc5e4b4e87a09271839ac031b8bae6c99be38
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2026-01-04 21:02:38 +0300

    Update media.ts

--- src/media.ts
+++ src/media.ts
@@ -1,16 +1,19 @@
-export const IMAGES = [
-  require("./Assets/Img/lv_0_20251229200212.png"),
-  require("./Assets/Img/lv_0_20251229200231.png"),
-  require("./Assets/Img/lv_0_20251229200259.png"),
-  require("./Assets/Img/lv_0_20251229200429.png"),
-  require("./Assets/Img/lv_0_20251229200448.png"),
-  require("./Assets/Img/lv_0_20251229200505.png"),
-];
+import { staticFile } from "remotion";
 
-export const VIDEOS = [
-  require("./Assets/Vid/lv_0_20260104111423.mp4"),
-  require("./Assets/Vid/lv_0_20260104111534.mp4"),
-  require("./Assets/Vid/lv_0_20260104111717.mp4"),
-  require("./Assets/Vid/lv_0_20260104111925.mp4"),
-];
+export type MediaItem =
+  | { type: "image"; src: string }
+  | { type: "video"; src: string };
 
+export const MEDIA: MediaItem[] = [
+  { type: "image", src: staticFile("img/lv_0_20251229200212.png") },
+  { type: "image", src: staticFile("img/lv_0_20251229200231.png") },
+  { type: "image", src: staticFile("img/lv_0_20251229200259.png") },
+  { type: "image", src: staticFile("img/lv_0_20251229200429.png") },
+  { type: "image", src: staticFile("img/lv_0_20251229200448.png") },
+  { type: "image", src: staticFile("img/lv_0_20251229200505.png") },
+
+  { type: "video", src: staticFile("vid/lv_0_20260104111423.mp4") },
+  { type: "video", src: staticFile("vid/lv_0_20260104111534.mp4") },
+  { type: "video", src: staticFile("vid/lv_0_20260104111717.mp4") },
+  { type: "video", src: staticFile("vid/lv_0_20260104111925.mp4") },
+];
