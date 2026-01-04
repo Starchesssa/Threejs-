53c12b6b3ce3d7506dbdad1a04a35f45db587f32
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2026-01-04 12:30:25 +0300

    Create media.ts

---
+++ src/media.ts
@@ -0,0 +1,16 @@
+export const IMAGES = [
+  require("./Assets/Img/lv_0_20251229200212.png"),
+  require("./Assets/Img/lv_0_20251229200231.png"),
+  require("./Assets/Img/lv_0_20251229200259.png"),
+  require("./Assets/Img/lv_0_20251229200429.png"),
+  require("./Assets/Img/lv_0_20251229200448.png"),
+  require("./Assets/Img/lv_0_20251229200505.png"),
+];
+
+export const VIDEOS = [
+  require("./Assets/Vid/lv_0_20260104111423.mp4"),
+  require("./Assets/Vid/lv_0_20260104111534.mp4"),
+  require("./Assets/Vid/lv_0_20260104111717.mp4"),
+  require("./Assets/Vid/lv_0_20260104111925.mp4"),
+];
+
