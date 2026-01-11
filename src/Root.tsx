77a057aaaabc927132444e5d597a7a4dd9a8edcb
Starchesssa <144559781+Starchesssa@users.noreply.github.com>
2026-01-11 23:05:06 +0300

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,21 +1,17 @@
+
+import React from 'react';
 import { Composition } from 'remotion';
 import { Scene } from './Scene';
 
-/**
- * 🎯 CONFIG (edit ONLY this section)
- */
-const FPS = 60;           // constant 60fps
-const DURATION_SEC = 12;  // 👈 duration in seconds
-
-export const Root = () => {
+export const Root: React.FC = () => {
   return (
     <Composition
       id="CinematicScene"
       component={Scene}
       width={1920}
       height={1080}
-      fps={FPS}
-      durationInFrames={FPS * DURATION_SEC} // ✅ seconds → frames
+      fps={60}
+      durationInFrames={12 * 60} // 12 seconds
     />
   );
 };
